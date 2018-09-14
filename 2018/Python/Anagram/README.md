5a2172ab3bced8cdb4b61bee54e68aa9cacfc619
Kishore Gudepu <43252066+kishoreggnv@users.noreply.github.com>
2018-09-14 14:57:44 -0500

    Anagrams (#2)

---
+++ 2018/Python/Anagram/README.md
@@ -0,0 +1,9 @@
+# Determines if two strings are anagrams
+
+Example:
+
+Enter the first string : developer
+
+Enter the second string : reDevelop
+
+Anagrams
---
+++ 2018/Python/Anagram/anagram.py
@@ -0,0 +1,4 @@
+# cerner_2^5_2018
+# Determine if two strings are anagrams
+
+print ('Anagrams' if sorted((input('Enter the first string :')).lower()) == sorted((input('Enter the second string :')).lower()) else 'Not anagrams')
